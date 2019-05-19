11308d82b3b217ce429b0014ba27312587bf17ab
louis baptiste <louisvis@ymail.com>
2019-05-19 04:35:53 +0000

    Done.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+    config.web_console.development_only = false
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
